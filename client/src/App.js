787147fec556b0764e6feb65d8be7cffa0620e33
Exper <18101184+TheJustExper@users.noreply.github.com>
2021-06-17 15:44:56 +0100

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import './css/App.css';
-import "./Game.js";
+import './styles/index.scss';
+
+import Header from "./components/header/header";
 
 export default function App() {
   return (
     <div id="app">
-     
+      <Header/>
     </div>
   );
 }
---
+++ client/src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./header.style.scss";
+
+export default () => {
+    return (
+        <header>
+            <div className="head">
+                <h1>Dylans Blog</h1>
+            </div>
+        </header>
+    )
+}
